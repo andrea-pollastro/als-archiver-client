a34df121f61678742650b7a662371ff185774877
Andrea Pollastro <andrea.poll93@gmail.com>
2025-05-05 16:26:01 +0200

    Update README.md with detailed documentation and examples; modify requirements.txt to use version ranges; fix setup.py URL to point to GitHub repository

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="archivertools", # Replace with your username
+    name="archivertools",
     version="1.0.0",
     author="Andrea Pollastro",
     author_email="apollastro@lbl.gov",
-    description="archivertools is a module to interact with the archiver server through Python.",
-    url="https://git.als.lbl.gov/physics/researchdev/archivertools",
+    description="archivertools is a module to interact with the ALS archiver server through Python.",
+    url="https://github.com/andrea-pollastro/als-archiver-client",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.10',
