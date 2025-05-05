075ce2bc124dd8e159b6054965bddbc32fee8623
Andrea Pollastro <andrea.poll93@gmail.com>
2025-05-05 17:10:06 +0200

    Bump version to 1.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="als-archiver-client",
-    version="1.0.0",
+    version="1.0.1",
     author="Andrea Pollastro",
     author_email="apollastro@lbl.gov",
     description="A Python package for interacting with the ALS archiver.",
